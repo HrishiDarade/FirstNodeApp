a26f89d40c4f668367e5b36e2c8f0eef2f11297e
Hrishikesh Darade <99548417+HrishiDarade@users.noreply.github.com>
2025-07-24 12:36:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,9 @@ app.get('/', (req, res) => {
       <body>
         <h1>Hello World</h1>
         <p>Testing CI/CD Deployment</p>
-        <img src="/image.jpg" alt="Test Image" />
+        <img src="/image.jpg" alt="Local Test Image" />
+        <br />
+        <img src="https://as2.ftcdn.net/jpg/06/30/90/99/1000_F_630909929_xHWmhNMPob3F5JDJE9Cfdjg6d80WBv6S.webp" alt="External Image" />
       </body>
     </html>
   `);
