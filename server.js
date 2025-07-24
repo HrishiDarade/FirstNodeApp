44e37cd6aede4c7f1031df354016bcea7b2d383f
Hrishikesh Darade <99548417+HrishiDarade@users.noreply.github.com>
2025-07-24 14:01:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,25 +23,28 @@ app.get('/', (req, res) => {
         <style>
           body {
             font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
+            background-color: #000;
+            color: #00BFFF
             text-align: center;
             padding: 50px;
           }
           h1 {
-            color: #333;
+            color: #00BFFF;
+          }
+          p{
+             color: #00BFFF;
           }
           img {
             width: 300px;
             margin-top: 20px;
             border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+            box-shadow: 0 0 10px rgba(0,0,0,0.5);
           }
         </style>
       </head>
       <body>
         <h1>Hello World</h1>
         <p>Testing CI/CD Deployment</p>
-        <img src="/image.jpg" alt="Local Test Image" />
         <br />
         <img src="https://as2.ftcdn.net/jpg/06/30/90/99/1000_F_630909929_xHWmhNMPob3F5JDJE9Cfdjg6d80WBv6S.webp" alt="External Image" />
       </body>
